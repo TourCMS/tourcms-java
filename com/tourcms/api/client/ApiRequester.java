ce7c9b7e1b42b926aab15bffd9e121c7427a648d
paulslugocki <paulslugocki@gmail.com>
2016-10-28 13:47:57 +0100

    ApiRequester support for POST

--- com/tourcms/api/client/ApiRequester.java
+++ com/tourcms/api/client/ApiRequester.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIUtils;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -57,38 +59,79 @@ public class ApiRequester {
 
       String uri = scheme + "://" + host + path;
 
-      HttpGet httpGet = new HttpGet(uri);
-      httpGet.setHeader("x-tourcms-date", currDate);
-      String sUri = httpGet.getURI().toString();
+      if(verb == "POST") {
+        HttpPost httpDetails = new HttpPost(uri);
+        HttpEntity postEntity = new ByteArrayEntity(postData.getBytes("UTF-8"));
+        httpDetails.setEntity(postEntity);
+        httpDetails.setHeader("x-tourcms-date", currDate);
+        String sUri = httpDetails.getURI().toString();
 
-      sUri = sUri.substring(23);
-      logger.info(sUri);
-      String authString = sg.encode(marketplaceId, apiKey, sUri, verb, channelId, time);
-      logger.info(authString);
-      httpGet.setHeader("Authorization", "TourCMS " + channelId + ":"+marketplaceId+":"+authString);
+        sUri = sUri.substring(23);
+        logger.info(sUri);
+        String authString = sg.encode(marketplaceId, apiKey, sUri, verb, channelId, time);
+        logger.info(authString);
+        httpDetails.setHeader("Authorization", "TourCMS " + channelId + ":"+marketplaceId+":"+authString);
+        httpDetails.setHeader("Content-type", "text/xml;charset=\"utf-8\"");
 
-      CloseableHttpClient httpclient = HttpClientBuilder.create().build();
-      HttpResponse response = null;
-      try {
-         response = httpclient.execute(httpGet);
+        CloseableHttpClient httpclient = HttpClientBuilder.create().build();
+        HttpResponse response = null;
+        try {
+           response = httpclient.execute(httpDetails);
+        }
+        catch (IOException ioe) {
+           //logger.error("error sending request: " + ioe.getMessage());
+           throw new Exception(ioe.getMessage(), ioe);
+        }
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+           try {
+              body = EntityUtils.toString(entity);
+              httpDetails.abort();
+           }
+           catch (IOException ioe2) {
+  //            logger.error("error extracting entity from post response: " + ioe2.getMessage());
+
+           }
+        }
+
+         return body;
+      } else {
+        HttpGet httpDetails = new HttpGet(uri);
+        httpDetails.setHeader("x-tourcms-date", currDate);
+        String sUri = httpDetails.getURI().toString();
+
+        sUri = sUri.substring(23);
+        logger.info(sUri);
+        String authString = sg.encode(marketplaceId, apiKey, sUri, verb, channelId, time);
+        logger.info(authString);
+        httpDetails.setHeader("Authorization", "TourCMS " + channelId + ":"+marketplaceId+":"+authString);
+        httpDetails.setHeader("Content-type", "text/xml;charset=\"utf-8\"");
+
+        CloseableHttpClient httpclient = HttpClientBuilder.create().build();
+        HttpResponse response = null;
+        try {
+           response = httpclient.execute(httpDetails);
+        }
+        catch (IOException ioe) {
+           //logger.error("error sending request: " + ioe.getMessage());
+           throw new Exception(ioe.getMessage(), ioe);
+        }
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+           try {
+              body = EntityUtils.toString(entity);
+              httpDetails.abort();
+           }
+           catch (IOException ioe2) {
+  //            logger.error("error extracting entity from post response: " + ioe2.getMessage());
+
+           }
+        }
+
+         return body;
       }
-      catch (IOException ioe) {
-         //logger.error("error sending request: " + ioe.getMessage());
-         throw new Exception(ioe.getMessage(), ioe);
-      }
-      HttpEntity entity = response.getEntity();
-      if (entity != null) {
-         try {
-            body = EntityUtils.toString(entity);
-            httpGet.abort();
-         }
-         catch (IOException ioe2) {
-//            logger.error("error extracting entity from post response: " + ioe2.getMessage());
 
-         }
-      }
 
-       return body;
    }
 
    /**
